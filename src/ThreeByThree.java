abe42dcd04a4c239e01c3bf5dcac49b48e0d5416
Lucy <thomalu@ssfss.org>
2020-03-09 14:15:04 -0400

    working on 2D arrays

---
+++ src/ThreeByThree.java
@@ -0,0 +1,24 @@
+public class ThreeByThree {
+        public static void main(String [] args){
+
+//code to define an initially empty double
+//2D array size 3x3
+
+            double [][] list = new double[3][3];
+
+//code to output all original values
+
+
+//code to change five of the values to positive
+
+
+//and the other four to negative
+
+
+//code to output a blank line separating output sets
+
+
+//code to output all array elements
+        }
+    }
+

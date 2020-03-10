3ec3b0563b7563a8d83da3ea26adb782cecc93bc
Lucy <thomalu@ssfss.org>
2020-03-10 15:36:27 -0400

    finished 2D arrays

---
+++ src/FourbyFour.java
@@ -0,0 +1,60 @@
+public class FourbyFour {
+    public static void main(String [] args) {
+        int [][] list = new int[4][4];
+        System.out.println(list[0][0]);
+        System.out.println(list[0][1]);
+        System.out.println(list[0][2]);
+        System.out.println(list[0][3]);
+        System.out.println(list[1][0]);
+        System.out.println(list[1][1]);
+        System.out.println(list[1][2]);
+        System.out.println(list[1][3]);
+        System.out.println(list[2][0]);
+        System.out.println(list[2][1]);
+        System.out.println(list[2][2]);
+        System.out.println(list[2][3]);
+        System.out.println(list[3][0]);
+        System.out.println(list[3][1]);
+        System.out.println(list[3][2]);
+        System.out.println(list[3][3]);
+
+        System.out.println();
+
+        list[0][0] = 2;
+        list[0][1] = 4;
+        list[0][2] = 6;
+        list[0][3] = 8;
+        list[1][0] = 10;
+        list[1][1] = 12;
+        list[1][2] = 14;
+        list[1][3] = 16;
+        list[2][0] = -1;
+        list[2][1] = -3;
+        list[2][2] = -5;
+        list[2][3] = -7;
+        list[3][0] = -9;
+        list[3][1] = -11;
+        list[3][2] = -13;
+        list[3][3] = -15;
+
+        System.out.println();
+
+        System.out.println(list[0][0]);
+        System.out.println(list[0][1]);
+        System.out.println(list[0][2]);
+        System.out.println(list[0][3]);
+        System.out.println(list[1][0]);
+        System.out.println(list[1][1]);
+        System.out.println(list[1][2]);
+        System.out.println(list[1][3]);
+        System.out.println(list[2][0]);
+        System.out.println(list[2][1]);
+        System.out.println(list[2][2]);
+        System.out.println(list[2][3]);
+        System.out.println(list[3][0]);
+        System.out.println(list[3][1]);
+        System.out.println(list[3][2]);
+        System.out.println(list[3][3]);
+    }
+
+}
--- src/ThreeByThree.java
+++ src/ThreeByThree.java
@@ -1,20 +1,40 @@
 public class ThreeByThree {
         public static void main(String [] args){
 
-//code to define an initially empty double
-//2D array size 3x3
+    double [][] list = new double[3][3];
+    System.out.println(list[0][0]);
+    System.out.println(list[0][1]);
+    System.out.println(list[0][2]);
+    System.out.println(list[1][0]);
+    System.out.println(list[1][1]);
+    System.out.println(list[1][2]);
+    System.out.println(list[2][0]);
+    System.out.println(list[2][1]);
+    System.out.println(list[2][2]);
 
-            double [][] list = new double[3][3];
+    System.out.println();
 
-//code to output all original values
+    list[0][0] = 1;
+    list[0][1] = 2;
+    list[0][2] = 3;
+    list[1][0] = 4;
+    list[1][1] = 5;
+    list[1][2] = -1;
+    list[2][0] = -2;
+    list[2][1] = -3;
+    list[2][2] = -4;
 
+    System.out.println();
 
-//code to change five of the values to positive
-
-
-//and the other four to negative
-
-
+    System.out.println(list[0][0]);
+    System.out.println(list[0][1]);
+    System.out.println(list[0][2]);
+    System.out.println(list[1][0]);
+    System.out.println(list[1][1]);
+    System.out.println(list[1][2]);
+    System.out.println(list[2][0]);
+    System.out.println(list[2][1]);
+    System.out.println(list[2][2]);
 //code to output a blank line separating output sets
 
 
